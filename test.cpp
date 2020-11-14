217d283835d584784abb28415c366f14c1f06226
Iris <wangming_ch@163.com>
2020-11-14 16:22:49 +0800

    update

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include <memory>
+#include <iostream>
+#include <functional>
+
+struct Services
+{
+    bool updateDeviceDriver()
+    {
+        return false;
+    }
+
+    bool bcnSync()
+    {
+        return false;
+    }
+};
+
+int main()
+{
+    return 0;
+}
